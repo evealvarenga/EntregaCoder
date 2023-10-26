cd7600a45d4d680cf52b5b814b069a3c56169219
evealvarenga <ealvarenga095@gmail.com>
2023-10-25 21:59:54 -0300

    Update ProductManager.js

--- src/manager/ProductManager.js
+++ src/manager/ProductManager.js
@@ -1,82 +1,30 @@
-import { promises, existsSync } from 'fs'
+import { productsModel } from '../db/models/products.model.js'
 
 class ProductManager {
-    constructor(path) {
-        this.path = path
-    }
 
     async addProduct(product) {
-        try {
-            const products = await this.getProducts()
-            let id
-            if (!products.length) {
-                id = 1
-            } else {
-                id = products[products.length - 1].id + 1
-            }
-            const newProduct = { id, status:true, ...product };
-            products.push(newProduct);
-            await promises.writeFile(this.path, JSON.stringify(products))
-            return newProduct
-        } catch (error) {
-            return error
-        }
+        const result = await productsModel.create(product);
+        return result;
     }
 
-    async getProducts(queryObj) {
-        const { limit } = queryObj;
-        try {
-            if (existsSync(this.path)) {
-                const productFile = await promises.readFile(this.path, 'utf-8')
-                const productData = JSON.parse(productFile);
-                return limit ? productData.slice(0, +limit) : productData;
-            } else {
-                return []
-            }
-        } catch (error) {
-            return error
-        }
+    async getProducts() {
+        const result = await productsModel.find().lean();
+        return result;
     }
 
     async getProductsByID(id) {
-        try {
-            const products = await this.getProducts()
-            const product = products.find(u => u.id === id)
-            return product;
-        } catch (error) {
-            return error
-        }
+        const result = await productsModel.findById(id);
+        return result;
     }
 
     async updateProduct(id, updatedProduct) {
-        try {
-            const products = await this.getProducts({});
-            const productIndex = products.findIndex(u => u.id === id);
-
-            if (productIndex === -1) {
-                return null;
-            }
-            const uProduct = { ...products[productIndex], ...updatedProduct};
-            products.splice(index, 1, uProduct);
-            await promises.writeFile(this.path, JSON.stringify(products));
-            return uProduct;
-        } catch (error) {
-            return error;
-        }
+        const result = await productsModel.updateOne({__id: id}, updatedProduct);
+        return result;
     }
 
     async deleteProduct(id) {
-        try {
-            const products = await this.getProducts()
-            const product = products.find(p => u.id ===id);
-            if(user){
-                const newArrayProducts = products.filter(u => u.id !== id)
-                await promises.writeFile(this.path, JSON.stringify(newArrayProducts))
-            }
-            return user;
-        } catch (error) {
-            return error
-        }
+        const result = await productsModel.deleteOne(id);
+        return result;
     }
 
 }
