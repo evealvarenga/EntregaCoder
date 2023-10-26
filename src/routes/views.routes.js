19043d68f4aafe48c928e9bc02543f348f84d33f
evealvarenga <ealvarenga095@gmail.com>
2023-10-25 22:23:25 -0300

    Update views.routes.js

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -2,11 +2,11 @@ import { Router } from "express";
 import ProductManager from '../manager/ProductManager.js';
 
 const router = Router()
-const manager = new ProductManager('./src/productos.json');
+const manager = new ProductManager();
 
 router.get("/", async (req, res) => {
     try {
-        const products = await manager.getProducts(req.query);
+        const products = await manager.getProducts();
         res.render("home", {response: products});
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -16,7 +16,7 @@ router.get("/", async (req, res) => {
 
 router.get("/realtimeproducts", async (req, res) => {
     try {
-        const products = await manager.getProducts(req.query);
+        const products = await manager.getProducts();
         res.render("realTimeProducts", {response: products});
     } catch (error) {
         res.status(500).json({ message: error.message });
