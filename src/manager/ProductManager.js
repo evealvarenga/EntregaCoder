3bea540ec19eff6aa56f3b732b04057508412bad
evealvarenga <ealvarenga095@gmail.com>
2023-11-01 19:43:34 -0300

    Update ProductManager.js

--- src/manager/ProductManager.js
+++ src/manager/ProductManager.js
@@ -2,31 +2,31 @@ import { productsModel } from '../db/models/products.model.js'
 
 class ProductManager {
 
-    async addProduct(product) {
+    async createOne(product) {
         const result = await productsModel.create(product);
         return result;
     }
 
-    async getProducts() {
+    async findAll() {
         const result = await productsModel.find().lean();
         return result;
     }
 
-    async getProductsByID(id) {
+    async findById(id) {
         const result = await productsModel.findById(id);
         return result;
     }
 
-    async updateProduct(id, updatedProduct) {
+    async updateOne(id, updatedProduct) {
         const result = await productsModel.updateOne({__id: id}, updatedProduct);
         return result;
     }
 
-    async deleteProduct(id) {
+    async deleteOne(id) {
         const result = await productsModel.deleteOne(id);
         return result;
     }
 
 }
 
-export default ProductManager;
+export const productManager = new ProductManager();
