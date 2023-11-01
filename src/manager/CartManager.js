000a1107c0c8b1e7d217b3e352ceea8aa3cd3982
evealvarenga <ealvarenga095@gmail.com>
2023-11-01 20:01:08 -0300

    Update CartManager.js

--- src/manager/CartManager.js
+++ src/manager/CartManager.js
@@ -13,22 +13,24 @@ class CartManager {
     }
 
     async getCartByID(id) {
-        const response = await cartsModel.findById(id)
+        const response = await cartsModel.findById(id).populate("products.product", ["name", "price"])
         return response;
     }
 
     async addProductToCart(idCart, idProduct){
         const cart = await cartsModel.findById(idCart);
-        const productIndex = cart.products.findIndex((p) => p.product === idProduct);
-        if (productIndex === -1 ){
-            cart.products.push({product: idProduct, quantity: 1});
+        if (!idCart){
+            return ("No encontramos el carrito indicado.")
         } else {
-            cart.products[productIndex].quantity++;
+            const productIndex = cart.products.findIndex((p) => p.product.equals(idProduct));
+            if (productIndex === -1 ){
+                cart.products.push({product: idProduct, quantity: 1});
+            } else {
+                cart.products[productIndex].quantity++;
+            }
+            return cart.save()
         }
-        return cart.save()
-
     }
-
 }
 
-export default CartManager;
+export const cartManager = new CartManager();
