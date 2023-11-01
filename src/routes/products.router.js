fd45a6bb17cbde0862cf313fd969d7881a870cc4
evealvarenga <ealvarenga095@gmail.com>
2023-11-01 19:51:07 -0300

    Update products.router.js

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -23,40 +23,30 @@ router.get('/:pid', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-    const { tittle, description, code, price, status, stock, category, thumbnails} =req.body
-    if (!tittle || !description || !code || !price || !status || !stock || !category) {
-        return res.status(400).json({message: "Some data is missing."})
-    }
     try {
-        const response = await productManager.addProduct(req.body);
+        const response = await productManager.createOne(req.body);
         res.status(200).json({message:"Producto creado", product: response})
     } catch (error) {
         res.status(500).json({message: error.message})
     }
 });
 
-//Endpoint para actualizar un producto
 router.put('/:pid', async (req, res) => {
-    const { pid } = req.params;
+    
     try {
+        const { pid } = req.params;
         const response = await productManager.updateProduct(+pid, req.body);
-        if(!response) {
-            return res.status(404).json({message:"Producto no encontrado con el ID indicado."})
-        }
         res.status(200).json ({message:"Producto modificado."})
     } catch (error) {
         res.status(500).json({message: error.message})
     }
 });
 
-//Endpoint para eliminar un producto
+
 router.delete('/:pid', async (req, res) => {
     const { pid } = req.params;
     try {
-        const response = await productManager.deleteProduct(+pid);
-        if (!response) {
-            return res.status(404).json({message: "Producto no encontrado con el ID indicado."})
-        }
+        await productManager.deleteOne(+pid);
         res.status(200).json({message: "Producto eliminado"})
     } catch (error) {
         res.status(500).json({message: error.message})
