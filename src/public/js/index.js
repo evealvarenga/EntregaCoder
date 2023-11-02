88aef4ec2f795976de2d417d3eddb4cfd70fe180
evealvarenga <ealvarenga095@gmail.com>
2023-11-01 21:31:20 -0300

    corrección index

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,30 +1,15 @@
 const socketClient = io();
 
-const formAddProduct = document.getElementById('form-add')
-const formDelProdcut = document.getElementById('form-delet')
+const form = document.getElementById("form");
+const iprice = document.getElementById("price")
+const pricePro = document.getElementById("pricePro")
 
-const title = document.getElementById('title')
-const description = document.getElementById('description')
-const code = document.getElementById('code')
-const price = document.getElementById('price')
-const stock = document.getElementById('stock')
-const number = document.getElementById('number')
-
-formAddProduct.onsubmit =  (e) => {
-    e.preventDefault()
-    const product = {
-        title: title.value,
-        description: description.value,
-        code: code.value,
-        price: price.value,
-        stock: stock.value,
-    }
-    socketClient.emit('product', product)
+form.onsubmit = (e) => {
+    e.preventDefault();
+    const price = iprice.value;
+    socketClient.emit("newPrice", price)
 }
 
-formDelProdcut.onsubmit =  (e) => {
-    e.preventDefault()
-    let id
-    id = number.value
-    socketClient.emit('id', id)
-}
+socketClient.on("priceUpdated", (price) => {
+    pricePro.innerText = price[0].title;
+})
